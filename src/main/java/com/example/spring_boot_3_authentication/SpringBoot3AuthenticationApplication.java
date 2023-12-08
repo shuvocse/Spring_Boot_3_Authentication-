c8f153847eee1bc0c2deb4955bd3c9d02134631e
DESKTOP-0IFSRJJ\Shuvo <shuvocse.uits@gmail.com>
2023-12-08 01:48:06 +0100

    Project initilization

---
+++ src/main/java/com/example/spring_boot_3_authentication/SpringBoot3AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_3_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3AuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3AuthenticationApplication.class, args);
+    }
+
+}
