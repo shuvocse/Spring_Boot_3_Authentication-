db32cdadd37a2cf82bd6fa0c2dc1b53bdde6aa9d
DESKTOP-0IFSRJJ\Shuvo <shuvocse.uits@gmail.com>
2023-12-08 17:03:11 +0100

    Authentication done

---
+++ src/main/java/com/example/spring_boot_3_authentication/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.example.spring_boot_3_authentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return"home";
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return"index";
+    }
+}
